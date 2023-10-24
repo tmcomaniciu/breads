93ea635d6d6a4158b3fec05de218822eedd1a95c
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-10-23 21:23:10 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,7 +4,10 @@ const Bread = require("../models/bread.js");
 
 // INDEX
 breads.get("/", (req, res) => {
-  res.send(Bread);
+  res.render("Index", {
+    breads: Bread,
+  });
+  // res.send(Bread)
 });
 
 // SHOW
